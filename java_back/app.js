1402b0fae4fcf2d1c1f7b03e8b775265e1901878
claumiranda <claudiasoledadmiranda16@gmail.com>
2024-10-27 21:05:15 -0300

    se agrega modo oscuro en el back

--- java_back/app.js
+++ java_back/app.js
@@ -48,3 +48,22 @@ function calculateTotal() {
     // Mostrar el total
     document.getElementById('total').value = `$${totalPrice.toFixed(2)}`;
 }
+
+// modo oscuro
+const temaOscuro = () => {
+//función que se encarga del tema del cambio a modo oscuro 
+  document.querySelector("body").setAttribute("data-bs-theme", "dark");
+  document.querySelector("#dl-icon").setAttribute("class", "bi bi-sun-fill");
+}
+
+const temaClaro = () => {
+  document.querySelector("body").setAttribute("data-bs-theme", "light");
+  document.querySelector("#dl-icon").setAttribute("class", "bi bi-moon-stars-fill");
+}
+
+// alternancia
+const cambiarTema = () => {
+  document.querySelector("body").getAttribute("data-bs-theme") === "light"?
+  temaOscuro() : temaClaro();
+}
+
